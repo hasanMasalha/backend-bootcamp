9305aa908d1758d18ebb19fe11ee806e89bad303
hasanMasalha <106591427+hasanMasalha@users.noreply.github.com>
2024-02-18 14:40:32 +0200

    final covertor

--- 18.02.24/convertor.py
+++ 18.02.24/convertor.py
@@ -6,13 +6,16 @@ match scource:
     case 'F':
          print((val-32)/1.8)
     case 'MPH':
-         print(val*1.609)
+         print(val/1.6)
     case 'KPH':
-        print(val/1.609)
-   #case KG:
-   #     action-1
-   #case stone:
-   #     action-2
-   #case lbs:
-   #     action-3
+          print(val*1.6)
+    case 'KG':
+         print( "stone" + val/6.5)
+         print("lbs" + val*2.204)
+    case 'stone':
+          print( "lbs" + val/0.071)
+          print("KG" + val/0.15)
+    case 'lbs':
+          print( "stone" + val/14)
+          print("KG" + val/2.204)
  
